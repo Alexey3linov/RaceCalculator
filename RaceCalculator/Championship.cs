aafc238b5f7a05c5fbfc5bfd0e68422fa9a58cdf
AXXONSOFT\alexey.blinov <alexey.blinov@axxonsoft.dev>
2023-08-15 19:34:36 +0400

    Add project files.

---
+++ RaceCalculator/Championship.cs
@@ -0,0 +1,24 @@
+﻿namespace RaceCalculator
+{
+    public class Championship
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        /// <summary>
+        /// Дата проведения
+        /// </summary>
+        public DateTime StartDate { get; set; }
+        
+        public IEnumerable<Stage> StageList { get; set; }
+        public int StageCount { get => StageList.Count(); }
+    }
+
+
+
+
+
+    public class Stage
+    {
+        public Guid Id { get; set; }
+    }
+}
