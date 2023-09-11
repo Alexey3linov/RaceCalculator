67a7f84773d1d0ca9a9d5f5e69a7bc0acd4d75a2
AXXONSOFT\alexey.blinov <alexey.blinov@axxonsoft.dev>
2023-09-11 19:27:56 +0400

    a little bit di

---
+++ RaceCalculator.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+
+using MediatR;
+
+namespace RaceCalculator.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ RaceCalculator.Persistence/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+using Microsoft.EntityFrameworkCore;
+
+using RaceCalculator.Application.Interfaces;
+
+namespace RaceCalculator.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<ChampionshipDbContext>(options =>
+            {
+                options.UseSqlite(connectionString);
+            });
+            services.AddScoped<IChampionshipDbContext>(provider => provider.GetService<ChampionshipDbContext>()!);
+            return services;
+        }
+    }
+}
