c62396974ec2309f781e3494906f35470362ee39
AXXONSOFT\alexey.blinov <alexey.blinov@axxonsoft.dev>
2023-09-26 19:31:22 +0400

    Corrected program.cs

--- RaceCalculator.WebApi/Program.cs
+++ RaceCalculator.WebApi/Program.cs
@@ -1,12 +1,34 @@
 using System.Reflection;
 
+using Microsoft.AspNetCore.Hosting;
+
 using RaceCalculator.Application;
 using RaceCalculator.Application.Common.Mappings;
 using RaceCalculator.Application.Interfaces;
 using RaceCalculator.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-var app     = builder.Build();
+
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IChampionshipDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration.Get<IConfiguration>());
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.AllowAnyOrigin();
+    });
+});
+
+var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
@@ -22,13 +44,13 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-app.Run();
-
-builder.Services.AddAutoMapper(config =>
+// pipeline
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseEndpoints(endpoints =>
 {
-    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
-    config.AddProfile(new AssemblyMappingProfile(typeof(IChampionshipDbContext).Assembly));
+    endpoints.MapControllers();
 });
 
-builder.Services.AddApplication();
-builder.Services.AddPersistence(builder.Configuration.Get<IConfiguration>());
+app.Run();
